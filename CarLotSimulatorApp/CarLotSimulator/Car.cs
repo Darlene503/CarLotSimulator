19b9b87e136f92ebfe438cad5f849dd351abc398
darlene503 <duyn503@gmail.com>
2023-07-31 21:40:51 -0700

    creat class file for car lot simulator

---
+++ CarLotSimulatorApp/CarLotSimulator/Car.cs
@@ -0,0 +1,22 @@
+﻿using System;
+namespace CarLotSimulator
+{
+    public class Car
+    {
+        
+            //Create a seperate class file called Car
+            //Car shall have the following properties: Year, Make, Model, EngineNoise, HonkNoise, IsDriveable
+            //Car shall have the following methods: MakeEngineNoise(), MakeHonkNoise()
+            //The methods should take one string parameter: the respective noise property
+
+        public int Year { get; set; }
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public string EngineNoise { get; set; }
+        public string HonkNoise { get; set; }
+        public bool IsDriveable { get; set; }
+        
+    
+    }
+}
+
